aa5ecd206ffecc1c8598f22fb9899bbfbb2dc2fa
tphat0127 <tphat0127@gmail.com>
2022-11-18 01:27:17 +0700

    validate

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatTableModule } from '@angular/material/table'
-import { FormsModule } from '@angular/forms';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input'
+import {MatTooltipModule} from '@angular/material/tooltip'
+
 import {
   NbSelectModule
 } from '@nebular/theme';
 
 import { ComboboxComponent } from './combobox/combobox.component';
 import { TableSampleComponent } from './table-sample/table-sample.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [ComboboxComponent, TableSampleComponent],
@@ -16,7 +24,13 @@ import { TableSampleComponent } from './table-sample/table-sample.component';
     NbSelectModule,
     CommonModule,
     FormsModule,
-    MatTableModule
+    MatTableModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTooltipModule
   ]
 })
 export class ComponentsModule { }
--- src/app/components/table-sample/table-sample.component.ts
+++ src/app/components/table-sample/table-sample.component.ts
@@ -1,55 +1,174 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
-import {MatTableDataSource} from '@angular/material/table';
+import { AfterViewInit, Component, ViewChild, OnInit } from '@angular/core';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
 import {MatPaginator} from '@angular/material/paginator';
 
 import { Subscription } from 'rxjs';
 import { TableSampleService } from '../../../_core/services/table-sample.service';
+import { FormBuilder, FormGroup,FormControl,FormArray,AbstractControl, Validators } from '@angular/forms';
+
+
+export interface UserElement { 
+  email: string;
+  id: number;
+  phone: string;
+  username: string;
+  password: string;
+}
+
+const ELEMENT_DATA: UserElement[] = [
+{
+  
+  "id": 1,
+  "email": "john@gmail.com",
+  "username": "johnd",
+  "password": "m38rmF$",
+  "phone": "1-570-236-7033",
+},
+{
+  
+  "id": 2,
+  "email": "john@gmail.com",
+  "username": "phat dz",
+  "password": "m38rmF$",
+  "phone": "1-570-236-7033",
+},
+  
+]
 
 @Component({
   selector: 'ngx-table-sample',
   templateUrl: './table-sample.component.html',
   styleUrls: ['./table-sample.component.scss']
 })
-export class TableSampleComponent implements AfterViewInit {
+
+export class TableSampleComponent implements OnInit {
 
   //Delaretion
   private subData: Subscription; //Observable: xu ly bat dong bo
-  constructor(private dataService: TableSampleService) {
-    this.getData();
+  pageNumber: number = 1;
+  dataSource = new MatTableDataSource<any>();
+  form: FormGroup;
+  isEditableNew: boolean = true;
+  data = Object.assign( ELEMENT_DATA);
+  submitted = false;
+
+  constructor(
+    private dataService: TableSampleService, 
+    private fb: FormBuilder,
+    private _formBuilder: FormBuilder) {
+    
+    //this.getData();
   }
 
-  displayedColumns: string[] = ['id', 'email', 'username', 'password', 'phone'];
-
-  //dataSource = new MatTableDataSource<UserElement>(this.resultArr);
-  dataSource : UserElement[] = [];
-  isLoadingResults = true;
-  isRateLimitReached = false;
-
-  @ViewChild(MatPaginator) paginator: MatPaginator;
+  displayedColumns: string[] = ['id', 'email', 'username', 'password', 'phone', 'action'];
 
   ngAfterViewInit() {
-    
-    //this.dataSource.paginator = this.paginator;
+
   }
+
+  ngOnInit(): void {
+    this.form = this._formBuilder.group({
+      Rows: this._formBuilder.array([])
+    });
+
+    this.form = this.fb.group({
+      Rows: this.fb.array(ELEMENT_DATA.map(val => this.fb.group({
+          id: new FormControl(val.id, Validators.required),
+          email: new FormControl(val.email),
+          phone: new FormControl(val.phone),
+          username: new FormControl(val.username),
+          password: new FormControl(val.password),
+          action: new FormControl('existingRecord'),
+          isEditable: new FormControl(true),
+          isNewRow: new FormControl(false),
+        })
+      )) //end of fb array
+    }); // end of form group cretation
+
+    this.dataSource = new MatTableDataSource((this.form.get('Rows') as FormArray).controls);
+    this.dataSource.paginator = this.paginator;
+
+    const filterPredicate = this.dataSource.filterPredicate;
+    this.dataSource.filterPredicate = (data: AbstractControl, filter) => {
+      return filterPredicate.call(this.dataSource, data.value, filter);
+    }
+  }
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+
   getData() {
     const uri = `users`;     
     this.subData = this.dataService.get(uri).subscribe((res: any) => {   //subscribe: Observable có thay đổi tự động gọi callback
-      this.isLoadingResults = false;
-      //this.resultArr = res;
       this.dataSource = res;
+      console.log(this.dataSource )
     });
+  }
+
+  AddNewRow() {
+    const control = this.form.get('Rows') as FormArray;
+    control.insert(0,this.initiateVOForm());
+    this.dataSource = new MatTableDataSource(control.controls)
+  }
+
+  deleteInline(FormElement, i) {
+    const control = this.form.get('Rows') as FormArray;
+    //control.removeAt(i);
+    //this.data.splice(i,1)
+    //this.dataSource = new MatTableDataSource(control.controls)
+   //FormElement.get('Rows').at(i).removeAt(i)
+   //this.dataSource = new MatTableDataSource(data)
+   this.dataSource = new MatTableDataSource(control.controls)
+   console.log(i);
+   console.log(FormElement.get('Rows').at(i).get('username').value)
+
+   this.data.splice(i,1);
+   //this.dataSource = new MatTableDataSource(data)
+  }
+
+  editInline(FormElement, i) {
+
+    FormElement.get('Rows').at(i).get('isEditable').patchValue(false);
+  }
+
+  // get f(): FormGroup {
+  //   return  this.fb
+  // }
+
+
+  // On click of correct button in table (after click on edit) this method will call
+  saveInline(FormElement, i) {
+    console.log(this.form.invalid)
+
+    FormElement.get('Rows').at(i).setErrors({'incorrect': true});
+
+    FormElement.get('Rows').at(i).get('isEditable').patchValue(true);
+    this.submitted = true;
+    console.log(FormElement.get('Rows').at(i).get('id').errors);
+    
+  }
+
+  // On click of cancel button in the table (after click on edit) this method will call and reset the previous data
+  cancelInline(FormElement, i) {
+    FormElement.get('Rows').at(i).get('isEditable').patchValue(true);
   }
 
   ngOnDestroy() {           //ngOnDestroy: chạy khi huy~ component(chuyen page,...)
     this.subData.unsubscribe();
   }
 
+  initiateVOForm(): FormGroup {
+    return this.fb.group({
+
+      id: new FormControl(234),
+      email: new FormControl(''),
+      phone: new FormControl(''),
+      username: new FormControl(''),
+      password: new FormControl(''),
+      action: new FormControl('newRecord'),
+      isEditable: new FormControl(false),
+      isNewRow: new FormControl(true),
+    });
+  }
+
 }
 
-export interface UserElement {
-  email: string;
-  id: number;
-  phone: number;
-  username: string;
-  password: string;
-}
